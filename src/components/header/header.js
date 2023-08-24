4217d993f4c03214981148fab572582a41e6362b
CorbinSpence <corbinspence16@gmail.com>
2023-08-23 22:48:17 -0400

    added fade-in animation for website

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,13 +4,15 @@ import './header.css'
 export default function header(){
     return(
         <header>
-            <span className="title">Corbin Spence</span>
-            <nav>
-                <a href="#About Me" className='navLinks'>About Me</a>
-                <a href="#Work" className='navLinks'>Work</a>
-                <a href="#Contact Info" className='navLinks'>Contact Me</a>
-                <a href="#Resume" className='navLinks'>Resume</a>
-            </nav>
+            <div className="relative-box">
+                <span className="title">Corbin Spence</span>
+                <nav>
+                    <a href="#About Me" className='navLinks'>About Me</a>
+                    <a href="#Work" className='navLinks'>Work</a>
+                    <a href="#Contact Info" className='navLinks'>Contact Me</a>
+                    <a href="#Resume" className='navLinks'>Resume</a>
+                </nav>
+            </div>
         </header>
     )
 }
